4eb055ad5ed00e1cec7f1449b98e99c65163ac64
dyuriaws <54648620+dyuriaws@users.noreply.github.com>
2020-10-27 21:54:25 -0400

    Update aws-console-masker.user.js

--- aws-console-masker.user.js
+++ aws-console-masker.user.js
@@ -13,18 +13,22 @@
     'use strict';
 
     // Replace logged in principal, account name, and federated user:
-    document.querySelector("#nav-usernameMenu > div.nav-elt-label").innerHTML = "AWS Account";
-    document.querySelector("#awsc-login-display-name-account").innerHTML = "My AWS Account";
-    document.querySelector("#awsc-login-display-name-user").innerHTML = "AnyCompany/jdoe";
+   // document.querySelector("#nav-usernameMenu > div.nav-elt-label").innerHTML = "AWS Account";
+    //document.querySelector("#awsc-login-display-name-account").innerHTML = "My AWS Account";
+   // document.querySelector("#awsc-login-display-name-user").innerHTML = "AnyCompany/jdoe";
 
     // Array of terms to replace, you can use Regular expressions.
 
     var replaceArry = [
-        [/([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})/, 'johndoe@example.com'], // email address
+        [/([a-zA-Z0-9_\-\.\+]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})/, 'johndoe@example.com'], // email address
         [/\d{12}/, '123456789012'], // 12 digit AWS Account #s
+        [/\#\d{12}/, '#123456789012'], // 12 digit AWS Account #s
+        [/([a-z0-9]{4})-([a-z0-9]{4})-([a-z0-9]{4})/, '1234-5678-9012'], // 12 digit AWS Account #s
         [/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/, '10.24.34.0'], // IP Addresses
         [/\b\d{1,3}\-\d{1,3}\-\d{1,3}\-\d{1,3}\b/, '10-24-34-0'], // EC2 DNS CNAME IPs
         [/(^|[^A-Z0-9])[A-Z0-9]{20}(?![A-Z0-9])/, 'AIDACKCEVSQ6C2EXAMPLE'], // IAM Access Key ID
+        [/([a-z0-9]{4})-([a-z0-9]{4})-([a-z0-9]{12})/, '1234-5678-9012-abcdefabcdef'],
+         [/(Isengard)|(ISENGARD)|(isengard)|(Nacunda)|(GatedGarden)|(gatedgarden)/, ''],
         // etc.
     ];
     var numTerms = replaceArry.length;
@@ -35,17 +39,19 @@
         /*--- Replace text on the page without busting links or javascript
         functionality.
     */
+
         var txtWalker = document.createTreeWalker(
             document.body,
             NodeFilter.SHOW_TEXT, {
                 acceptNode: function (node) {
                     //-- Skip whitespace-only nodes
                     if (node.nodeValue.trim()) {
-                        if (node.tmWasProcessed)
-                            return NodeFilter.FILTER_SKIP;
-                        else
+                        if (node.tmWasProcessed) {
+                           return NodeFilter.FILTER_SKIP;
+                        } else{
                             return NodeFilter.FILTER_ACCEPT;
-                    }
+                        }
+                   }
                     return NodeFilter.FILTER_SKIP;
                 }
             },
@@ -53,22 +59,57 @@
         );
         var txtNode = null;
         while (txtNode = txtWalker.nextNode()) {
+           // console.log(txtNode,txtNode.nodeValue);
             txtNode.nodeValue = replaceAllTerms(txtNode.nodeValue);
+
             txtNode.tmWasProcessed = true;
         }
         //
         //--- Now replace user-visible attributes.
         //
+
         var placeholderNodes = document.querySelectorAll("[placeholder]");
         replaceManyAttributeTexts(placeholderNodes, "placeholder");
 
         var titleNodes = document.querySelectorAll("[title]");
         replaceManyAttributeTexts(titleNodes, "title");
+
+        if (!window.frames[0]) {
+         return 0;
+        }
+        txtWalker = null;
+        txtWalker = window.frames[0].document.createTreeWalker(
+            window.frames[0].document.body,
+            NodeFilter.SHOW_TEXT, {
+                acceptNode: function (node) {
+                    //-- Skip whitespace-only nodes
+                    if (node.nodeValue.trim()) {
+                        if (node.tmWasProcessed) {
+                           return NodeFilter.FILTER_SKIP;
+                        } else{
+                            return NodeFilter.FILTER_ACCEPT;
+                        }
+                   }
+                    return NodeFilter.FILTER_SKIP;
+                }
+            },
+            false
+        );
+        txtNode = null;
+        while (txtNode = txtWalker.nextNode()) {
+          //  console.log(txtNode,txtNode.nodeValue);
+            txtNode.nodeValue = replaceAllTerms(txtNode.nodeValue);
+
+            txtNode.tmWasProcessed = true;
+        }
+        
     }
 
     function replaceAllTerms(oldTxt) {
         for (var J = 0; J < numTerms; J++) {
+
             oldTxt = oldTxt.replace(replaceArry[J][0], replaceArry[J][1]);
+
         }
         return oldTxt;
     }
@@ -77,11 +118,13 @@
         for (var J = nodeList.length - 1; J >= 0; --J) {
             var node = nodeList[J];
             var oldText = node.getAttribute(attributeName);
+
             if (oldText) {
                 oldText = replaceAllTerms(oldText);
                 node.setAttribute(attributeName, oldText);
-            } else
+            } else {
                 throw "attributeName does not match nodeList in replaceManyAttributeTexts";
+            }
         }
     }
 
